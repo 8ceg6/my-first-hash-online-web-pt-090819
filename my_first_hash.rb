ff7a342337f2e165bbd65603ed7ef1231cccfbcc
cedric e goddard <8ceg6@comcast.net>
2019-10-02 18:30:24 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  # use the literal constructor to set the variable, my_hash, equal to a #hash with key/value pairs of your choice.
     states_lived= {"state 1" => "Tennessee" , "state 2" => "Virginia"}
   
 end
