6fcf1321d08383c9d3cad3c74d7cdeb44158fbec
cedric e goddard <8ceg6@comcast.net>
2019-10-02 18:33:25 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a #hash with key/value pairs of your choice.
-    your_hash= {"state 1" => "Tennessee" , "state 2" => "Virginia"}
+  
+    your_hash= {"state1" => "Tennessee" , "state2" => "Virginia"}
   
 end
 
