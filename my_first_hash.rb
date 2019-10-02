61496806637bf0113688b230f1cebb5b966a1bb5
cedric e goddard <8ceg6@comcast.net>
2019-10-02 18:32:05 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,7 @@ def my_hash
   
 end
 
-
+my_hash
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
