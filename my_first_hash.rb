9f81aa87181b8c4e8388d75eb74ce13541207e14
cedric e goddard <8ceg6@comcast.net>
2019-10-02 18:24:27 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,7 +23,7 @@ def retrieval
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
-
+shipping_manifest[oil paintings]
 def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
@@ -31,11 +31,11 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-shipping_manifest{muskets}=2
+shipping_manifest[muskets]=2
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+shipping_manifest[gun_powder]=4 
 
 
   # return the shipping_manifest hash below
